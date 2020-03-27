dd1baa50f7fb6787a479bb776b851cc4fa3907e2
Colin Alexander <colintalex@gmail.com>
2020-03-27 02:08:44 -0600

    Updates test with RSpec framework

--- spec/features/favorites_list/index_spec.rb
+++ spec/features/favorites_list/index_spec.rb
@@ -1,12 +1,20 @@
+require 'rails_helper'
 
- it "When I click the favorites indicator in the nav bar I am taken to favorites index" do
-   visit '/shelters'
+RSpec.describe "As a visitor" type: :feature do
+  describe "when I have added pets to my favorites list" do
+    it "When I click the favorites indicator in the nav bar I am taken to favorites index" do
+      visit '/shelters'
 
-   within('.navbar') do
-     within('.favorites_counter')
-      click_link 'Favorites:'
-     end
-   end
+      within('.navbar') do
+        within('.favorites_counter')
+        click_link 'Favorites:'
+      end
 
-   expect(current_path).to eql('/favorites')
- end
+      expect(current_path).to eql('/favorites')
+    end
+  end
+
+  describe "when I have no pets in my favorites list" do
+    
+  end
+end
