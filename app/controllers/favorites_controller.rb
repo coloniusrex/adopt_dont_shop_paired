091d3ea9e7f85dddf290400dde2bd93341e2d598
Colin Alexander <colintalex@gmail.com>
2020-03-27 18:47:58 -0600

    Completes User Story 12

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,4 +2,11 @@ class FavoritesController < ApplicationController
   def index
     @favorite_pets = Pet.where(id: favorites_list.pets)
   end
+
+  def destroy
+    favorites_list.remove_id(params[:pet_id])
+    session[:favorites_list] = favorites_list.pets
+    flash[:notice] = "Successfully Removed Pet from Favorites"
+    redirect_to "/pets/#{params[:pet_id]}"
+  end
 end
