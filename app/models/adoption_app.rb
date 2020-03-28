8dd70980d677fa5d7d443b659fe6c990fedc20b3
Ryan Camp <campryan@comcast.net>
2020-03-28 13:33:25 -0600

    Add validations to AdoptionApp model, Pet model, and PetAdoptionModel

--- app/models/adoption_app.rb
+++ app/models/adoption_app.rb
@@ -1,2 +1,7 @@
 class AdoptionApp < ApplicationRecord
+  validates_presence_of :name, :address, :city, :state, :zip, :phone_number,
+                        :description
+
+  has_many :pet_adoption_apps
+  has_many :pets, through: :pet_adoption_apps
 end
--- app/models/pet_adoption_app.rb
+++ app/models/pet_adoption_app.rb
@@ -1,2 +1,4 @@
 class PetAdoptionApp < ApplicationRecord
+  belongs_to :pet
+  belongs_to :adoption_app
 end
