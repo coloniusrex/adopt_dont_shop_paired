638526ea4233eb16288c0537dae416f3507873ed
Ryan Camp <campryan@comcast.net>
2020-03-28 13:21:27 -0600

    Create bare bones models for adoption app and joins, create migrations

---
+++ app/models/adoption_app.rb
@@ -0,0 +1,2 @@
+class AdoptionApp < ApplicationRecord
+end
---
+++ db/migrate/20200328192046_create_pet_adoption_app.rb
@@ -0,0 +1,8 @@
+class CreatePetAdoptionApp < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pet_adoption_apps do |t|
+      t.references :adoption_app, foreign_key: true
+      t.references :pet, foreign_key: true
+    end
+  end
+end
