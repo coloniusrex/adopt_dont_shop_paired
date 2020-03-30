963336f082453d296477fb4c441302e40b8d4059
Colin Alexander <colintalex@gmail.com>
2020-03-29 19:28:26 -0600

    Sotry 24 Test and method for one approved app at a time

--- spec/features/adoption_apps/show_spec.rb
+++ spec/features/adoption_apps/show_spec.rb
@@ -176,4 +176,98 @@ RSpec.describe "On adoption apps show page", type: :feature do
     end
 
   end
+
+  it "I can no longer approve an app if another app is currently approved on this pet" do
+    application1 = AdoptionApp.create(name:"Ryan",
+                                    address: "23 Cedarwood Road",
+                                    city: "Omaha",
+                                    state: "NE",
+                                    zip: "68107",
+                                    phone_number: "456-908-7656",
+                                    description: "I am a good pet owner")
+
+    application2 = AdoptionApp.create(name:"Colin",
+                                    address: "8397 Mayfair Lane",
+                                    city: "Chevy Chase",
+                                    state: "MD",
+                                    zip: "20815",
+                                    phone_number: "303-675-0987",
+                                    description: "I am the best pet owner")
+
+    shelter_1 = Shelter.create(name:    "Foothills Animal Shelter",
+                               address: "580 McIntyre St",
+                               city:    "Golden",
+                               state:   "CO",
+                               zip:     "80401")
+    pet_1 = shelter_1.pets.create(image_url:        "https://images.unsplash.com/photo-1518900673653-cf9fdd01e430?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+                                   name:            "Tom",
+                                   description:     "Squirrel",
+                                   approximate_age: "4",
+                                   sex:             "Male",
+                                   adoptable:       true,)
+
+    application1.process([pet_1.id])
+    application2.process([pet_1.id])
+
+    visit "/adoption_apps/#{application1.id}"
+
+    within("#app-pet-#{pet_1.id}") do
+      click_link 'Approve Application'
+    end
+
+    visit "/adoption_apps/#{application2.id}"
+
+    within("#app-pet-#{pet_1.id}") do
+      expect(page).to have_no_link('Approve Application')
+      expect(page).to have_content('Another application is currently pending.')
+    end
+  end
+
+  it "I can unapprove an application and see the pets show page adoption status has changed" do
+    application = AdoptionApp.create(name:"Colin",
+                                    address: "8397 Mayfair Lane",
+                                    city: "Chevy Chase",
+                                    state: "MD",
+                                    zip: "20815",
+                                    phone_number: "303-675-0987",
+                                    description: "I am the best pet owner")
+
+    shelter_1 = Shelter.create(name:    "Foothills Animal Shelter",
+                               address: "580 McIntyre St",
+                               city:    "Golden",
+                               state:   "CO",
+                               zip:     "80401")
+    pet_1 = shelter_1.pets.create(image_url:        "https://images.unsplash.com/photo-1518900673653-cf9fdd01e430?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+                                   name:            "Tom",
+                                   description:     "Squirrel",
+                                   approximate_age: "4",
+                                   sex:             "Male",
+                                   adoptable:       true,)
+
+    application.process([pet_1.id])
+
+    visit "/adoption_apps/#{application.id}"
+
+    within("#app-pet-#{pet_1.id}") do
+      click_link 'Approve Application'
+    end
+
+    visit "/adoption_apps/#{application.id}"
+
+    within("#app-pet-#{pet_1.id}") do
+      click_link 'Un-Approve Application'
+    end
+
+    expect(current_path).to eql("/adoption_apps/#{application.id}")
+
+    within("#app-pet-#{pet_1.id}") do
+      expect(page).to have_link('Approve Application')
+    end
+
+    visit "/pets/#{pet_1.id}"
+
+    within('.pet-show-info') do
+      expect(page).to have_content("#{pet_1.name} is adoptable")
+    end
+  end
 end
