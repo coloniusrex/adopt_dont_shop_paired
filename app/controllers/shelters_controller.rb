184951f436ddac3458f470600dfdcb5359fc841e
Colin Alexander <colintalex@gmail.com>
2020-03-30 22:36:20 -0600

    Create missing fields flash for new shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -11,8 +11,14 @@ class SheltersController < ApplicationController
   end
 
   def create
-    Shelter.create(shelter_params)
-    redirect_to '/shelters'
+    shelter = Shelter.new(shelter_params)
+    if shelter.save
+      redirect_to '/shelters'
+    else
+      flash[:error] = "Incomplete Submission: Please fill out #{missing_fields} to complete your form."
+      redirect_to request.referer
+    end
+
   end
 
   def edit
@@ -35,5 +41,15 @@ class SheltersController < ApplicationController
   private
   def shelter_params
     params.permit(:name, :address, :city, :state, :zip)
+  end
+
+  def missing_fields
+    missing_hash = shelter_params.keep_if{|k,v| v == ""}
+    missing_keys = missing_hash.keys
+    capitalize(missing_keys).join(", ")
+  end
+
+  def capitalize(array)
+    array.map { |item| item.capitalize }
   end
 end
